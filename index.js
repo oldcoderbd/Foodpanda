29aa9697e316e7c8c28b64f889458bd2f16e9457
oldcoderbd <129336269+oldcoderbd@users.noreply.github.com>
2024-03-24 09:09:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ function cart(){
     def = def+1;
     document.getElementById("sup1").innerHTML = def;
 }
-let butt = document.querySelector(".but");
-butt.addEventListener("click", cart);
+document.querySelector(".but").addEventListener("click", cart);
